90fe3c878e5b7daebbbebee268f5e8c0458512dc
Uítalo <uitalo.urss@gmail.com>
2023-11-30 14:56:30 -0300

    configuração do linter

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
-import "reflect-metadata";
-import { app } from "./server";
-import { dataSource } from "./typeorm/dataSource";
+import 'reflect-metadata';
+import { app } from './server';
+import { dataSource } from './typeorm/dataSource';
 
 dataSource.initialize().then(() => {
-  app.listen(3000, () => console.log("Server is running on port 3000"));
+    app.listen(3000, () => console.log('Server is running on port 3000'));
 });
